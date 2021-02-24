d1ac92bd63c700d64839e38581d26a8f2c466858
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-02-23 19:35:31 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {App} from '../../IETILAB3-task-planner-app/src/App';
+import {App} from "./App";
+
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
